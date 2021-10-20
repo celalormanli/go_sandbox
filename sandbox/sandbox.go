5b7714ac94155f2ffbf5d54176d57405cb797f00
Celal ORMANLI <ormanlicelal@gmail.com>
2021-10-20 23:18:45 +0300

    tower of hanoi with tests

--- sandbox/sandbox.go
+++ sandbox/sandbox.go
@@ -6,3 +6,16 @@ func Fibonacci(x int) int {
 	}
 	return Fibonacci(x-1) + Fibonacci(x-2)
 }
+
+func TowerOfHanoi(height int, from_pole *[]int, to_pole *[]int, with_pole *[]int) {
+	if height >= 1 {
+		TowerOfHanoi(height-1, from_pole, with_pole, to_pole)
+
+		pop := (*from_pole)[(len(*from_pole) - 1)]
+		*from_pole = (*from_pole)[:len(*from_pole)-1]
+		*to_pole = append(*to_pole, pop)
+
+		TowerOfHanoi(height-1, with_pole, to_pole, from_pole)
+	}
+
+}
--- sandbox/sandbox_test.go
+++ sandbox/sandbox_test.go
@@ -1,6 +1,9 @@
 package sandbox
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestFibonacci(t *testing.T) {
 	if Fibonacci(8) != 21 {
@@ -8,5 +11,25 @@ func TestFibonacci(t *testing.T) {
 	}
 	if Fibonacci(10) != 55 {
 		t.Errorf("Fibonacci should be 55")
+	}
+}
+
+func TestTowerOfHanoi(t *testing.T) {
+	src := []int{4, 3, 2, 1}
+	aux := []int{}
+	dest := []int{}
+
+	var s *[]int = &src
+	var a *[]int = &aux
+	var d *[]int = &dest
+	TowerOfHanoi(4, s, d, a)
+	if !reflect.DeepEqual(*d, []int{4, 3, 2, 1}) {
+		t.Errorf("d should be [4, 3, 2, 1]")
+	}
+	if !reflect.DeepEqual(*a, []int{}) {
+		t.Errorf("a should be []")
+	}
+	if !reflect.DeepEqual(*s, []int{}) {
+		t.Errorf("s should be []")
 	}
 }
