70611e3d5d039f9badf23446f9600aecc8b32bfb
Celal ORMANLI <ormanlicelal@gmail.com>
2021-10-22 21:09:35 +0300

    binary search

--- searching_algorithms/searching_algorithms.go
+++ searching_algorithms/searching_algorithms.go
@@ -8,3 +8,18 @@ func LinearSearch(inputArray []int, searchedElement int) bool {
 	}
 	return false
 }
+
+func BinarySearch(inputArray []int, le int, ri int, x int) bool {
+	for le <= ri {
+		var mid int
+		mid = le + (ri-1)/2
+		if inputArray[mid] == x {
+			return true
+		} else if inputArray[mid] < x {
+			le = mid + 1
+		} else {
+			ri = mid - 1
+		}
+	}
+	return false
+}
--- searching_algorithms/searching_algorithms_test.go
+++ searching_algorithms/searching_algorithms_test.go
@@ -11,3 +11,13 @@ func TestLinearSearch(t *testing.T) {
 		t.Errorf("LinearSearch should return true")
 	}
 }
+
+func TestBinarySearch(t *testing.T) {
+	if BinarySearch([]int{8, 13, 58, 96, 101}, 0, 4, 5) == true {
+		t.Errorf("BinarySearch should return false")
+	}
+
+	if BinarySearch([]int{8, 13, 58, 96, 101}, 0, 4, 96) == false {
+		t.Errorf("BinarySearch should return true")
+	}
+}
